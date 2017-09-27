94a0d53b2b6e08fd525b0217bfdb2a61a18ec08f
aliveJus <danilka_S@mail.ru>
2017-09-27 19:49:58 +0300

    + verstka

---
+++ wordpress.php
@@ -0,0 +1,5 @@
+подключение CSS  в function.php
+--------------------------------------
+  wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.css', array(), '1.0.0', true ); //
+
+  ------------------------------------------
