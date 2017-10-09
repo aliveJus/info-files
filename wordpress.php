8155e8ca43a5ac8064b0b9285a8dd53ac263a7a5
aliveJus <danilka_S@mail.ru>
2017-10-09 10:40:35 +0300

    + еще что-то

--- wordpress.php
+++ wordpress.php
@@ -3,3 +3,8 @@
   wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.css', array(), '1.0.0', true ); //
 
   ------------------------------------------
+  Alethemes framework
+  -----------------------------------------
+assets - папка с css,images,js для админки
+etc - тоже для админки
+папка "functions" код подключает не в одной файле functions.php, а несколько файлов из папки
